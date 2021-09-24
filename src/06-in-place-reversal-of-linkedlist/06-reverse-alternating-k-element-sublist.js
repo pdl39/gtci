415c9cee0d43df4d2505d3550b87cb25ba423fda
pdl39 <dhlee9111@gmail.com>
2021-09-24 07:39:31 -0400

    Update - add alternate solution

--- src/06-in-place-reversal-of-linkedlist/06-reverse-alternating-k-element-sublist.js
+++ src/06-in-place-reversal-of-linkedlist/06-reverse-alternating-k-element-sublist.js
@@ -8,7 +8,10 @@ const Node = require('../../ds/Node');
 // S: O(1)
 // where n = # of nodes in the linked list.
 
-const reverseAlternatingKElementSublist = (head, k) => {
+// This question is very similar to 05-reverse-every-k-element-sublist, except that here we don't reverse every k-element sublist, but every other k-element sublist.
+
+// #1: Using reverseSublist function.
+const reverseAlternatingKElementSublists = (head, k) => {
   if (!head || k < 2) return head;
 
   const n = getLinkedlistLength(head);
@@ -74,6 +77,52 @@ const reverseSublist = (head, p, q) => {
 }
 
 
+// #2: A revised version of the reverseSublist function.
+const reverseAlternatingKElementSublists2 = (head, k) => {
+  if (!head || k < 2) return head;
+
+  let p = 1, q = k;
+
+  let current = head;
+  let prev = null;
+
+  while (current) {
+    let lastNodeBeforeSublist = prev;
+    let lastNodeOfReversedSublist = current;
+
+    while (current && p <= q) {
+      let next = current.next;
+      current.next = prev;
+      prev = current;
+      current = next;
+
+      p++;
+    }
+
+    // At this point, prev will be the first node of the reversed sublist & current will be the first node of the remaining linked list.
+    if (lastNodeBeforeSublist) {
+      lastNodeBeforeSublist.next = prev;
+    }
+    else {
+      head = prev;
+    }
+
+    // lastNodeBeforeSublist = lastNodeOfReversedSublist;
+    prev = lastNodeOfReversedSublist;
+    lastNodeOfReversedSublist.next = current;
+
+    // skip alternating k-elements.
+    while (current && p <= q + k) {
+      prev = current;
+      current = current.next;
+      p++;
+    }
+    q = p + k - 1;
+  }
+
+  return head;
+}
+
 
 // TEST
 const ex1Node = new Node(1);
@@ -100,91 +149,184 @@ ex2Node.next.next.next.next.next.next.next.next.next.next = new Node(11);
 ex2Node.next.next.next.next.next.next.next.next.next.next.next = new Node(12);
 
 
-const reversedEx1 = reverseAlternatingKElementSublist(ex1Node, 3);
+const reversedEx1 = reverseAlternatingKElementSublists(ex1Node, 3);
 console.log(reversedEx1.printDiagram());
-const revertedEx1 = reverseAlternatingKElementSublist(reversedEx1, 3);
+const revertedEx1 = reverseAlternatingKElementSublists(reversedEx1, 3);
 console.log(revertedEx1.printDiagram());
 
 console.log('------');
 
-const reversedEx2 = reverseAlternatingKElementSublist(ex1Node, 2);
+const reversedEx2 = reverseAlternatingKElementSublists(ex1Node, 2);
 console.log(reversedEx2.printDiagram());
-const revertedEx2 = reverseAlternatingKElementSublist(reversedEx2, 2);
+const revertedEx2 = reverseAlternatingKElementSublists(reversedEx2, 2);
 console.log(revertedEx2.printDiagram());
 
 console.log('------');
 
-const reversedEx3 = reverseAlternatingKElementSublist(ex1Node, 1);
+const reversedEx3 = reverseAlternatingKElementSublists(ex1Node, 1);
 console.log(reversedEx3.printDiagram());
-const revertedEx3 = reverseAlternatingKElementSublist(reversedEx3, 1);
+const revertedEx3 = reverseAlternatingKElementSublists(reversedEx3, 1);
 console.log(revertedEx3.printDiagram());
 
 console.log('------');
 
-const reversedEx4 = reverseAlternatingKElementSublist(ex1Node, 6);
+const reversedEx4 = reverseAlternatingKElementSublists(ex1Node, 6);
 console.log(reversedEx4.printDiagram());
-const revertedEx4 = reverseAlternatingKElementSublist(reversedEx4, 6);
+const revertedEx4 = reverseAlternatingKElementSublists(reversedEx4, 6);
 console.log(revertedEx4.printDiagram());
 
 console.log('------');
 
-const reversedEx5 = reverseAlternatingKElementSublist(ex1Node, 15);
+const reversedEx5 = reverseAlternatingKElementSublists(ex1Node, 15);
 console.log(reversedEx5.printDiagram());
-const revertedEx5 = reverseAlternatingKElementSublist(reversedEx5, 15);
+const revertedEx5 = reverseAlternatingKElementSublists(reversedEx5, 15);
 console.log(revertedEx5.printDiagram());
 
 console.log('------');
 
-const reversedEx6 = reverseAlternatingKElementSublist(ex1Node, 9);
+const reversedEx6 = reverseAlternatingKElementSublists(ex1Node, 9);
 console.log(reversedEx6.printDiagram());
-const revertedEx6 = reverseAlternatingKElementSublist(reversedEx6, 9);
+const revertedEx6 = reverseAlternatingKElementSublists(reversedEx6, 9);
 console.log(revertedEx6.printDiagram());
 
 console.log('------');
 
-const reversedEx7 = reverseAlternatingKElementSublist(ex1Node, 8);
+const reversedEx7 = reverseAlternatingKElementSublists(ex1Node, 8);
 console.log(reversedEx7.printDiagram());
-const revertedEx7 = reverseAlternatingKElementSublist(reversedEx7, 8);
+const revertedEx7 = reverseAlternatingKElementSublists(reversedEx7, 8);
 console.log(revertedEx7.printDiagram());
 
 console.log('------');
 
-const reversedEx8 = reverseAlternatingKElementSublist(ex2Node, 3);
+const reversedEx8 = reverseAlternatingKElementSublists(ex2Node, 3);
 console.log(reversedEx8.printDiagram());
-const revertedEx8 = reverseAlternatingKElementSublist(reversedEx8, 3);
+const revertedEx8 = reverseAlternatingKElementSublists(reversedEx8, 3);
 console.log(revertedEx8.printDiagram());
 
 console.log('------');
 
-const reversedEx9 = reverseAlternatingKElementSublist(ex2Node, 2);
+const reversedEx9 = reverseAlternatingKElementSublists(ex2Node, 2);
 console.log(reversedEx9.printDiagram());
-const revertedEx9 = reverseAlternatingKElementSublist(reversedEx9, 2);
+const revertedEx9 = reverseAlternatingKElementSublists(reversedEx9, 2);
 console.log(revertedEx9.printDiagram());
 
 console.log('------');
 
-const reversedEx10 = reverseAlternatingKElementSublist(ex2Node, 4);
+const reversedEx10 = reverseAlternatingKElementSublists(ex2Node, 4);
 console.log(reversedEx10.printDiagram());
-const revertedEx10 = reverseAlternatingKElementSublist(reversedEx10, 4);
+const revertedEx10 = reverseAlternatingKElementSublists(reversedEx10, 4);
 console.log(revertedEx10.printDiagram());
 
 console.log('------');
 
-const reversedEx11 = reverseAlternatingKElementSublist(ex2Node, 5);
+const reversedEx11 = reverseAlternatingKElementSublists(ex2Node, 5);
 console.log(reversedEx11.printDiagram());
-const revertedEx11 = reverseAlternatingKElementSublist(reversedEx11, 5);
+const revertedEx11 = reverseAlternatingKElementSublists(reversedEx11, 5);
 console.log(revertedEx11.printDiagram());
 
 console.log('------');
 
-const reversedEx12 = reverseAlternatingKElementSublist(ex2Node, 6);
+const reversedEx12 = reverseAlternatingKElementSublists(ex2Node, 6);
 console.log(reversedEx12.printDiagram());
-const revertedEx12 = reverseAlternatingKElementSublist(reversedEx12, 6);
+const revertedEx12 = reverseAlternatingKElementSublists(reversedEx12, 6);
 console.log(revertedEx12.printDiagram());
 
 console.log('------');
 
-const reversedEx13 = reverseAlternatingKElementSublist(ex2Node, 7);
+const reversedEx13 = reverseAlternatingKElementSublists(ex2Node, 7);
 console.log(reversedEx13.printDiagram());
-const revertedEx13 = reverseAlternatingKElementSublist(reversedEx13, 7);
+const revertedEx13 = reverseAlternatingKElementSublists(reversedEx13, 7);
 console.log(revertedEx13.printDiagram());
+
+/* ----------------------------------------------------- */
+console.log('\n==========================================\n');
+/* ----------------------------------------------------- */
+
+const reversed_2_Ex1 = reverseAlternatingKElementSublists2(ex1Node, 3);
+console.log(reversed_2_Ex1.printDiagram());
+const reverted_2_Ex1 = reverseAlternatingKElementSublists2(reversed_2_Ex1, 3);
+console.log(reverted_2_Ex1.printDiagram());
+
+console.log('------');
+
+const reversed_2_Ex2 = reverseAlternatingKElementSublists2(ex1Node, 2);
+console.log(reversed_2_Ex2.printDiagram());
+const reverted_2_Ex2 = reverseAlternatingKElementSublists2(reversed_2_Ex2, 2);
+console.log(reverted_2_Ex2.printDiagram());
+
+console.log('------');
+
+const reversed_2_Ex3 = reverseAlternatingKElementSublists2(ex1Node, 1);
+console.log(reversed_2_Ex3.printDiagram());
+const reverted_2_Ex3 = reverseAlternatingKElementSublists2(reversed_2_Ex3, 1);
+console.log(reverted_2_Ex3.printDiagram());
+
+console.log('------');
+
+const reversed_2_Ex4 = reverseAlternatingKElementSublists2(ex1Node, 6);
+console.log(reversed_2_Ex4.printDiagram());
+const reverted_2_Ex4 = reverseAlternatingKElementSublists2(reversed_2_Ex4, 6);
+console.log(reverted_2_Ex4.printDiagram());
+
+console.log('------');
+
+const reversed_2_Ex5 = reverseAlternatingKElementSublists2(ex1Node, 15);
+console.log(reversed_2_Ex5.printDiagram());
+const reverted_2_Ex5 = reverseAlternatingKElementSublists2(reversed_2_Ex5, 15);
+console.log(reverted_2_Ex5.printDiagram());
+
+console.log('------');
+
+const reversed_2_Ex6 = reverseAlternatingKElementSublists2(ex1Node, 9);
+console.log(reversed_2_Ex6.printDiagram());
+const reverted_2_Ex6 = reverseAlternatingKElementSublists2(reversed_2_Ex6, 9);
+console.log(reverted_2_Ex6.printDiagram());
+
+console.log('------');
+
+const reversed_2_Ex7 = reverseAlternatingKElementSublists2(ex1Node, 8);
+console.log(reversed_2_Ex7.printDiagram());
+const reverted_2_Ex7 = reverseAlternatingKElementSublists2(reversed_2_Ex7, 8);
+console.log(reverted_2_Ex7.printDiagram());
+
+console.log('------');
+
+const reversed_2_Ex8 = reverseAlternatingKElementSublists2(ex2Node, 3);
+console.log(reversed_2_Ex8.printDiagram());
+const reverted_2_Ex8 = reverseAlternatingKElementSublists2(reversed_2_Ex8, 3);
+console.log(reverted_2_Ex8.printDiagram());
+
+console.log('------');
+
+const reversed_2_Ex9 = reverseAlternatingKElementSublists2(ex2Node, 2);
+console.log(reversed_2_Ex9.printDiagram());
+const reverted_2_Ex9 = reverseAlternatingKElementSublists2(reversed_2_Ex9, 2);
+console.log(reverted_2_Ex9.printDiagram());
+
+console.log('------');
+
+const reversed_2_Ex10 = reverseAlternatingKElementSublists2(ex2Node, 4);
+console.log(reversed_2_Ex10.printDiagram());
+const reverted_2_Ex10 = reverseAlternatingKElementSublists2(reversed_2_Ex10, 4);
+console.log(reverted_2_Ex10.printDiagram());
+
+console.log('------');
+
+const reversed_2_Ex11 = reverseAlternatingKElementSublists2(ex2Node, 5);
+console.log(reversed_2_Ex11.printDiagram());
+const reverted_2_Ex11 = reverseAlternatingKElementSublists2(reversed_2_Ex11, 5);
+console.log(reverted_2_Ex11.printDiagram());
+
+console.log('------');
+
+const reversed_2_Ex12 = reverseAlternatingKElementSublists2(ex2Node, 6);
+console.log(reversed_2_Ex12.printDiagram());
+const reverted_2_Ex12 = reverseAlternatingKElementSublists2(reversed_2_Ex12, 6);
+console.log(reverted_2_Ex12.printDiagram());
+
+console.log('------');
+
+const reversed_2_Ex13 = reverseAlternatingKElementSublists2(ex2Node, 7);
+console.log(reversed_2_Ex13.printDiagram());
+const reverted_2_Ex13 = reverseAlternatingKElementSublists2(reversed_2_Ex13, 7);
+console.log(reverted_2_Ex13.printDiagram());
