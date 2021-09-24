907552c38822b14c9e8369dd77ce46336b43f8b3
pdl39 <dhlee9111@gmail.com>
2021-09-24 06:47:30 -0400

    Update - add alternate solution

--- src/06-in-place-reversal-of-linkedlist/05-reverse-every-k-element-sublist.js
+++ src/06-in-place-reversal-of-linkedlist/05-reverse-every-k-element-sublist.js
@@ -8,16 +8,16 @@ const Node = require('../../ds/Node');
 // S: O(1)
 // where n = # of nodes in the linked list.
 
+// This question is very similar to 02-reverse-a-sublist, except that here we reverse multiple k-length sublists, updating p and q as we go.
+
+// #1: Using the reverseSublist function.
+
 const reverseEveryKElements = (head, k) => {
-  if (k === 0) return head;
+  if (!head || k < 2) return head;
 
   const n = getLinkedlistLength(head);
 
-  let isKOne = k === 1;
-
-  let p = 1;
-  let q = isKOne ? n : k;
-
+  let p = 1, q = k;
 
   while (p <= n) {
     head = reverseSublist(head, p, q);
@@ -46,14 +46,7 @@ const reverseSublist = (head, p, q) => {
   let current = head;
   let prev = null;
 
-  /* We need to keep track of 3 different sections of the linked list:
-    1. head ~ last node before reverse
-    2. the sublist to reverse (first node to reverse ~ last node to reverse)
-    3. first node after reverse ~ tail
-  */
-
   let currentPosition = 1;
-  // move the current to point to the node at position p, and the prev to point to the node at p - 1.
   while (current && currentPosition < p) {
     prev = current;
     current = current.next;
@@ -61,11 +54,9 @@ const reverseSublist = (head, p, q) => {
     currentPosition++;
   }
 
-  // current should now be the node at position p, which should be the lastNodeOfReversedSublist. Note that the lastNodeOfReversedSublist is actually the first node of the mid section we are keeping track of, which will be the last node of the section after we reverse the section.
   const lastNodeOfFirstSection = prev;
   const lastNodeOfReversedSublist = current;
 
-  // until current reaches the node at position q, inclusive, we reverse the sublist in place and continue updating prev and current.
   while (current && currentPosition <= q) {
     let next = current.next;
     current.next = prev;
@@ -75,20 +66,54 @@ const reverseSublist = (head, p, q) => {
     currentPosition++;
   }
 
-  // At this point, prev is the last node of the sublist (or the first node of the reversed sublist), and current if the first node of the last section.
-  const firstNodeOfReversedSublist = prev;
-  const firstNodeOfLastSection = current;
-
-  // we need to re-connect the reversed sublist to each end of the linked list. As long as we have a non-null last node of first section, its next should be the first node of the reversed sublist. If there is no first section, it means the first node of the reversed sublist should be the head.
   if (lastNodeOfFirstSection) {
-    lastNodeOfFirstSection.next = firstNodeOfReversedSublist;
+    lastNodeOfFirstSection.next = prev;
   }
   else {
-    head = firstNodeOfReversedSublist;
+    head = prev;
   }
 
-  // connect the end of the reversed sublist to the last section. If there is no last section, firstNodeOfLastSection will already be null, so the following is all we need.
-  lastNodeOfReversedSublist.next = firstNodeOfLastSection;
+  lastNodeOfReversedSublist.next = current;
+
+  return head;
+}
+
+// #2: A revised version of the reverseSublist function.
+const reverseEveryKElements2 = (head, k) => {
+  if (!head || k < 2) return head;
+
+  let p = 1, q = k;
+
+  let current = head;
+  let prev = null;
+
+  while (current) {
+    let lastNodeBeforeSublist = prev;
+    let lastNodeOfReversedSublist = current;
+
+    while (current && p <= q) {
+      let next = current.next;
+      current.next = prev;
+      prev = current;
+      current = next;
+
+      p++;
+    }
+
+    // At this point, prev will be the first node of the reversed sublist & current will be the first node of the remaining linked list.
+    if (lastNodeBeforeSublist) {
+      lastNodeBeforeSublist.next = prev;
+    }
+    else {
+      head = prev;
+    }
+
+    // lastNodeBeforeSublist = lastNodeOfReversedSublist;
+    prev = lastNodeOfReversedSublist;
+    lastNodeOfReversedSublist.next = current;
+
+    q = p + k - 1;
+  }
 
   return head;
 }
@@ -130,4 +155,76 @@ const reversedEx4 = reverseEveryKElements(ex1Node, 6);
 console.log(reversedEx4.printDiagram());
 const revertedEx4 = reverseEveryKElements(reversedEx4, 6);
 console.log(revertedEx4.printDiagram());
+
+console.log('------');
+
+const reversedEx5 = reverseEveryKElements(ex1Node, 15);
+console.log(reversedEx5.printDiagram());
+const revertedEx5 = reverseEveryKElements(reversedEx5, 15);
+console.log(revertedEx5.printDiagram());
+
+console.log('------');
+
+const reversedEx6 = reverseEveryKElements(ex1Node, 9);
+console.log(reversedEx6.printDiagram());
+const revertedEx6 = reverseEveryKElements(reversedEx6, 9);
+console.log(revertedEx6.printDiagram());
+
+console.log('------');
+
+const reversedEx7 = reverseEveryKElements(ex1Node, 8);
+console.log(reversedEx7.printDiagram());
+const revertedEx7 = reverseEveryKElements(reversedEx7, 8);
+console.log(revertedEx7.printDiagram());
+
+/* ----------------------------------------------------- */
+console.log('\n==========================================\n');
+/* ----------------------------------------------------- */
+
+const reversed_2_Ex1 = reverseEveryKElements2(ex1Node, 3);
+console.log(reversed_2_Ex1.printDiagram());
+const reverted_2_Ex1 = reverseEveryKElements2(reversed_2_Ex1, 3);
+console.log(reverted_2_Ex1.printDiagram());
+
+console.log('------');
+
+const reversed_2_Ex2 = reverseEveryKElements2(ex1Node, 2);
+console.log(reversed_2_Ex2.printDiagram());
+const reverted_2_Ex2 = reverseEveryKElements2(reversed_2_Ex2, 2);
+console.log(reverted_2_Ex2.printDiagram());
+
+console.log('------');
+
+const reversed_2_Ex3 = reverseEveryKElements2(ex1Node, 1);
+console.log(reversed_2_Ex3.printDiagram());
+const reverted_2_Ex3 = reverseEveryKElements2(reversed_2_Ex3, 1);
+console.log(reverted_2_Ex3.printDiagram());
+
+console.log('------');
+
+const reversed_2_Ex4 = reverseEveryKElements2(ex1Node, 6);
+console.log(reversed_2_Ex4.printDiagram());
+const reverted_2_Ex4 = reverseEveryKElements2(reversed_2_Ex4, 6);
+console.log(reverted_2_Ex4.printDiagram());
+
+console.log('------');
+
+const reversed_2_Ex5 = reverseEveryKElements2(ex1Node, 15);
+console.log(reversed_2_Ex5.printDiagram());
+const reverted_2_Ex5 = reverseEveryKElements2(reversed_2_Ex5, 15);
+console.log(reverted_2_Ex5.printDiagram());
+
+console.log('------');
+
+const reversed_2_Ex6 = reverseEveryKElements2(ex1Node, 9);
+console.log(reversed_2_Ex6.printDiagram());
+const reverted_2_Ex6 = reverseEveryKElements2(reversed_2_Ex6, 9);
+console.log(reverted_2_Ex6.printDiagram());
+
+console.log('------');
+
+const reversed_2_Ex7 = reverseEveryKElements2(ex1Node, 8);
+console.log(reversed_2_Ex7.printDiagram());
+const reverted_2_Ex7 = reverseEveryKElements2(reversed_2_Ex7, 8);
+console.log(reverted_2_Ex7.printDiagram());
 
