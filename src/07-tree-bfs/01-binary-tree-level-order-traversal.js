524a9db26a13ffbd054c7160583945f983bab9df
pdl39 <dhlee9111@gmail.com>
2021-09-26 10:59:16 -0400

    Add 07-tree-bfs: 01-binary-tree-level-order-traversal

--- src/07-tree-bfs/01-binary-tree-level-order-traversal.js
+++ src/07-tree-bfs/01-binary-tree-level-order-traversal.js
@@ -1,1 +1,35 @@
 /* Given a binary tree, populate an array to represent its level-by-level traversal. You should populate the values of all nodes of each level from left to right in separate sub-arrays. */
+
+const Tree = require('../../ds/BinaryTree');
+const Queue = require('../../ds/Queue');
+
+// T: O(n) --> we need to traverse each tree node once.
+// S: O(n) --> n for the output array & n for the queue.
+// where n = # of nodes in the tree.
+
+const traverse = (root) => {
+  const levelOrderTree = [];
+  const queue = new Queue(root);
+
+  while (queue.length) {
+    const current = queue.dequeue().value; // the Queue stores each tree wrapped in a node, so to get the tree, we call the value property.
+    levelOrderTree.push(current.value);
+
+    if (current.left) queue.add(current.left);
+    if (current.right) queue.add(current.right);
+  }
+
+  return levelOrderTree;
+}
+
+
+// TEST
+const tree1 = new Tree(5);
+tree1.left = new Tree(8);
+tree1.left.left = new Tree(3);
+tree1.left.right = new Tree(4);
+tree1.right = new Tree(9);
+tree1.right.left = new Tree(11);
+
+
+console.log(traverse(tree1));
