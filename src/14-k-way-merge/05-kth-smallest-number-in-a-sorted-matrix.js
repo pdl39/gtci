428d13fdfaa8f0e3bf5a6556ccf91242e148e4ea
pdl39 <dhlee9111@gmail.com>
2021-10-18 03:59:33 -0400

    Update - add alternate solution

--- src/14-k-way-merge/05-kth-smallest-number-in-a-sorted-matrix.js
+++ src/14-k-way-merge/05-kth-smallest-number-in-a-sorted-matrix.js
@@ -11,12 +11,10 @@ Input: Matrix=[
 Output: 7
 Explanation: The 5th smallest number in the matrix is 7. */
 
-// T: O(klogn)
-// S: O(1)
-// where n = matrix dimension on each side (n x n matrix).
 
 const Heap = require('../../ds/PriorityQueue');
 
+// #1: Using min heap.
 // T: O(mlogm + klogm) --> We run through the lists up to m + k times - m times to set up the initial min heap of m elements and k times until we get the kth smallest number - and run add/poll operations - logm - in each iteration.
 // S: O(m) --> At any point, the min heap will contain at most m elements, one from each of the m arrays.
 // where m = # of sorted arrays, k = kth number to find.
@@ -48,6 +46,57 @@ const kthSmallestNumber = (matrix, k) => {
   return currentNum;
 }
 
+// #2: Using binary search.
+// T: O(nlog(max - min))
+// S: O(1)
+// where n = # of rows (or columns) in the matrix, max = the biggest number in the matrix, min = the smallest number in the matrix.
+
+const kthSmallestNumber2 = (matrix, k) => {
+  const n = matrix.length;
+  // instead of doing a binar search on the low/high indices, we will do a binary search on the low/high numbers. When we start off, the range will be from low = the upper left corner number of the matrix, which must be the smallest number of the matrix, and high = the lower right corner number, which must be the biggest number.
+  let low = matrix[0][0];
+  let high = matrix[n - 1][n - 1];
+
+  while (low < high) {
+    // The mid number is NOT necessarily a number in the matrix. I will be used as a reference point to search in the right half.
+    const mid = low + Math.floor((high - low) / 2);
+    const [count, leftEnd, rightStart] = getCountAndNewBounds(matrix, mid, matrix[0][0], matrix[n - 1][n - 1]);
+
+    if (count === k) return leftEnd;
+
+    if (count < k) {
+      low = rightStart;
+    }
+    else {
+      high = leftEnd;
+    }
+  }
+
+  return low;
+}
+
+const getCountAndNewBounds = (matrix, mid, leftEnd, rightStart) => {
+  const n = matrix.length;
+  let row = n - 1;
+  let col = 0;
+  let count = 0;
+
+  while (row >= 0 && col < n) {
+    // Since we start with the last row (n - 1) at the first index (col = 0), if the number at current position is bigger than mid, we know all other numbers in the current row at col > 0 will also be bigger than mid, so we look at a lower row. When we have reached a row with the number at col = 0 that is less than or equal to mid, we know we should look at other numbers in the current row as we have already looked at all higher rows before coming to the current row, all of whose numbers we know are bigger than mid. We also know that all numbers at col <= current col at any lower rows are all smaller than mid, so when we encounter a number bigger than mid, we can stay at the same col and look at a lower row. row and col will always move in one direction.
+    if (matrix[row][col] > mid) {
+      rightStart = Math.min(rightStart, matrix[row][col]);
+      row--;
+    }
+    else {
+      leftEnd = Math.max(leftEnd, matrix[row][col]);
+      col++;
+      count += row + 1;
+    }
+  }
+
+  return [count, leftEnd, rightStart];
+}
+
 
 // TEST
 const matrix1 = [
@@ -70,7 +119,7 @@ const matrix3 = [
   [11, 13, 15, 16],
   [21, 23, 25, 26],
   [31, 33, 35, 36]
-]
+];
 
 console.log(kthSmallestNumber(matrix1, 5));
 console.log(kthSmallestNumber(matrix1, 2));
@@ -85,3 +134,19 @@ console.log(kthSmallestNumber(matrix3, 5));
 console.log(kthSmallestNumber(matrix3, 2));
 console.log(kthSmallestNumber(matrix3, 6));
 console.log(kthSmallestNumber(matrix3, 16));
+
+console.log('---------------');
+
+console.log(kthSmallestNumber2(matrix1, 5));
+console.log(kthSmallestNumber2(matrix1, 2));
+console.log(kthSmallestNumber2(matrix1, 6));
+console.log(kthSmallestNumber2(matrix2, 1));
+console.log(kthSmallestNumber2(matrix2, 4));
+console.log(kthSmallestNumber2(matrix2, 18));
+console.log(kthSmallestNumber2(matrix2, 19));
+console.log(kthSmallestNumber2(matrix2, 25));
+console.log(kthSmallestNumber2(matrix2, 36));
+console.log(kthSmallestNumber2(matrix3, 5));
+console.log(kthSmallestNumber2(matrix3, 2));
+console.log(kthSmallestNumber2(matrix3, 6));
+console.log(kthSmallestNumber2(matrix3, 16));
