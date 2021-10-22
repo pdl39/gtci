fb910eb4f7b5ff1e2bbc815730a31dac10e0ed7c
pdl39 <dhlee9111@gmail.com>
2021-10-22 11:36:37 -0400

    Update - add memoization solution

--- src/15-dp/02-equal-subset-sum-partition.js
+++ src/15-dp/02-equal-subset-sum-partition.js
@@ -16,11 +16,14 @@ Input: {2, 3, 4, 6}
 Output: False
 Explanation: The given set cannot be partitioned into two subsets with equal sum. */
 
+// #1: Brute Force
 // T: O(2^n) --> 2 nested recursive calls
 // S: O(n) --> n for recursive stack.
 // where n = input array length.
 
-const equalSumPartition = (arr) => {
+// If there are to be two subsets of equal sums, it means each subset's sum will be exactly half of the total sum of all numbers. This means this question essentially translates to finding a subset whose sum equals s / 2 where s = total sum of all numbers.
+
+const equalSumPartitionBF = (arr) => {
   let sum = 0;
   arr.forEach(num => sum += num);
 
@@ -29,23 +32,72 @@ const equalSumPartition = (arr) => {
 }
 
 const canPartition = (arr, currentIdx, sum) => {
-  console.log(arr[currentIdx], 'sum: ', sum);
   if (currentIdx >= arr.length) return false;
   if (arr[currentIdx] === sum) return true;
 
   if (arr[currentIdx] < sum) {
+    // include the current number, only if it is smaller than the remaining sum.
     if (canPartition(arr, currentIdx + 1, sum - arr[currentIdx])) {
+      // if we ever get true, return true immediately.
       return true;
     }
   }
+  // skip current number
   return canPartition(arr, currentIdx + 1, sum);
 }
 
 
+// #2: Memoization
+// T: O(n * s) --> Since already visited combinations are returned directly from the dp matrix, we don't do any overlapping operations. We do n * s operations.
+// S: O(n * s) --> n * s for the dp matrix.
+// where n = input array length, s = total sum of all numbers.
+
+const equalSumPartitionMemo = (arr) => {
+  let sum = 0;
+  arr.forEach(num => sum += num);
+
+  const dp = new Array(arr.length).fill({})
+    .map(() => new Object());
+
+  if (sum % 2 !== 0) return false; // can't have two equal sum partition if sum is odd.
+  return canPartitionMemo(arr, 0, sum / 2, dp);
+}
+
+const canPartitionMemo = (arr, currentIdx, sum, dp) => {
+  if (currentIdx >= arr.length) return false;
+  if (arr[currentIdx] === sum) return true;
+
+  if (!dp[currentIdx][sum]) { // Only run recursive calls if the current number & sum combination hasn't yet been computed.
+    if (arr[currentIdx] < sum) {
+      // include the current number, only if it is smaller than the remaining sum.
+      dp[currentIdx][sum] = canPartitionMemo(arr, currentIdx + 1, sum - arr[currentIdx], dp); // save subproblem result.
+      if (dp[currentIdx][sum]) {
+        // if we ever get true, return true immediately.
+        return true;
+      }
+    }
+
+    // skip current number.
+    dp[currentIdx][sum] = canPartitionMemo(arr, currentIdx + 1, sum, dp); // save subproblem result.
+  }
+
+  return dp[currentIdx][sum];
+}
+
+
 // TEST
-console.log(equalSumPartition([1, 4, 2, 5, 3, 1, 2, 2]));
-console.log(equalSumPartition([1, 4, 2, 5, 3, 1, 2, 9]));
-console.log(equalSumPartition([1, 4, 2, 5, 3, 1, 2, 4]));
-console.log(equalSumPartition([1, 4, 2, 5, 3, 1, 2, 8]));
-console.log(equalSumPartition([1, 4, 6, 5]));
-console.log(equalSumPartition([1, 2, 3, 4]));
+console.log(equalSumPartitionBF([1, 4, 2, 5, 3, 1, 2, 2]));
+console.log(equalSumPartitionBF([1, 4, 2, 5, 3, 1, 2, 9]));
+console.log(equalSumPartitionBF([1, 4, 2, 5, 3, 1, 2, 4]));
+console.log(equalSumPartitionBF([1, 4, 2, 5, 3, 1, 2, 8]));
+console.log(equalSumPartitionBF([1, 4, 6, 5]));
+console.log(equalSumPartitionBF([1, 2, 3, 4]));
+
+console.log('--------------------');
+
+console.log(equalSumPartitionMemo([1, 4, 2, 5, 3, 1, 2, 2]));
+console.log(equalSumPartitionMemo([1, 4, 2, 5, 3, 1, 2, 9]));
+console.log(equalSumPartitionMemo([1, 4, 2, 5, 3, 1, 2, 4]));
+console.log(equalSumPartitionMemo([1, 4, 2, 5, 3, 1, 2, 8]));
+console.log(equalSumPartitionMemo([1, 4, 6, 5]));
+console.log(equalSumPartitionMemo([1, 2, 3, 4]));
