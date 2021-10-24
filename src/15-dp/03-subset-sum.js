785738c0eed5f55658428036c4f20d4791a431d4
pdl39 <dhlee9111@gmail.com>
2021-10-24 06:43:48 -0400

    Update - add memoization solution

--- src/15-dp/03-subset-sum.js
+++ src/15-dp/03-subset-sum.js
@@ -16,6 +16,9 @@ The given set does not have any subset whose sum is equal to '6'. */
 // This question is essentially the same as 02-equal-subset-sum-partition, except here, we are directly given the sum value to match instead of deriving it from the total sum of all numbers and dividing it by 2.
 
 // #1: Brute Force
+// T: O(2^n)
+// S: (n) --> for recursion stack
+// where n = input array length.
 const subsetSumBF = (arr, sum) => {
   return doesSubsetExist(arr, 0, sum);
 }
@@ -34,6 +37,38 @@ const doesSubsetExist = (arr, currentIdx, targetSum) => {
 }
 
 
+// #2: Memoization
+// T: O(n * s)
+// S: (n * s) --> for dp
+// where n = input array length, s = target sum.
+const subsetSumMemo = (arr, sum) => {
+  const dp = new Array(arr.length).fill(null)
+  .map(() => new Object());
+
+  // return doesSubsetExistMemo(arr, 0, sum, dp);
+  const result = doesSubsetExistMemo(arr, 0, sum, dp);
+  // console.log(dp);
+  return result;
+}
+
+const doesSubsetExistMemo = (arr, currentIdx, targetSum, dp) => {
+  if (targetSum === 0) return true;
+  if (currentIdx >= arr.length) return false;
+
+  if (targetSum in dp[currentIdx]) return dp[currentIdx][targetSum]; // return from dp if exists.
+
+  dp[currentIdx][targetSum] = doesSubsetExistMemo(arr, currentIdx + 1, targetSum, dp); // exclude the current number if it is bigger than the target sum or if the subset that included it returned false.
+
+  if (arr[currentIdx] <= targetSum) { // include the number in the subset if it is smaller than or equal to the target sum.
+    const subResult = doesSubsetExistMemo(arr, currentIdx + 1, targetSum - arr[currentIdx], dp);
+    if (subResult) dp[currentIdx][targetSum] = subResult;
+  }
+
+  return dp[currentIdx][targetSum];
+}
+
+
+
 // TEST
 console.log(subsetSumBF([1, 4, 2, 5, 3, 1, 2, 2], 6));
 console.log(subsetSumBF([1, 4, 2, 5, 3, 1, 2, 9], 24));
@@ -47,27 +82,36 @@ console.log(subsetSumBF([1, 2, 7, 1, 5], 10));
 
 console.log('--------------------');
 
-// console.log(subsetSumMemo([1, 4, 2, 5, 3, 1, 2, 2]));
-// console.log(subsetSumMemo([1, 4, 2, 5, 3, 1, 2, 9]));
-// console.log(subsetSumMemo([1, 4, 2, 5, 3, 1, 2, 4]));
-// console.log(subsetSumMemo([1, 4, 2, 5, 3, 1, 2, 8]));
-// console.log(subsetSumMemo([1, 4, 6, 5]));
-// console.log(subsetSumMemo([1, 2, 3, 4]));
+console.log(subsetSumMemo([1, 4, 2, 5, 3, 1, 2, 2], 6));
+console.log(subsetSumMemo([1, 4, 2, 5, 3, 1, 2, 9], 24));
+console.log(subsetSumMemo([1, 4, 2, 5, 3, 1, 2, 8], 35));
+console.log(subsetSumMemo([1, 4, 6, 5], 11));
+console.log(subsetSumMemo([1, 4, 6, 5], 3));
+console.log(subsetSumMemo([1, 2, 3, 4], 8));
+console.log(subsetSumMemo([1, 2, 3, 4], 10));
+console.log(subsetSumMemo([1, 3, 4, 8], 6));
+console.log(subsetSumMemo([1, 2, 7, 1, 5], 10));
+
+console.log('--------------------');
+
+// console.log(subsetSumBF([1, 4, 2, 5, 3, 1, 2, 2], 6));
+// console.log(subsetSumBF([1, 4, 2, 5, 3, 1, 2, 9], 24));
+// console.log(subsetSumBF([1, 4, 2, 5, 3, 1, 2, 8], 35));
+// console.log(subsetSumBF([1, 4, 6, 5], 11));
+// console.log(subsetSumBF([1, 4, 6, 5], 3));
+// console.log(subsetSumBF([1, 2, 3, 4], 8));
+// console.log(subsetSumBF([1, 2, 3, 4], 10));
+// console.log(subsetSumBF([1, 3, 4, 8], 6));
+// console.log(subsetSumBF([1, 2, 7, 1, 5], 10));
 
 // console.log('--------------------');
 
-// console.log(subsetSumTabular([1, 4, 2, 5, 3, 1, 2, 2]));
-// console.log(subsetSumTabular([1, 4, 2, 5, 3, 1, 2, 9]));
-// console.log(subsetSumTabular([1, 4, 2, 5, 3, 1, 2, 4]));
-// console.log(subsetSumTabular([1, 4, 2, 5, 3, 1, 2, 8]));
-// console.log(subsetSumTabular([1, 4, 6, 5]));
-// console.log(subsetSumTabular([1, 2, 3, 4]));
-
-// console.log('--------------------');
-
-// console.log(subsetSumSet([1, 4, 2, 5, 3, 1, 2, 2]));
-// console.log(subsetSumSet([1, 4, 2, 5, 3, 1, 2, 9]));
-// console.log(subsetSumSet([1, 4, 2, 5, 3, 1, 2, 4]));
-// console.log(subsetSumSet([1, 4, 2, 5, 3, 1, 2, 8]));
-// console.log(subsetSumSet([1, 4, 6, 5]));
-// console.log(subsetSumSet([1, 2, 3, 4]));
+// console.log(subsetSumBF([1, 4, 2, 5, 3, 1, 2, 2], 6));
+// console.log(subsetSumBF([1, 4, 2, 5, 3, 1, 2, 9], 24));
+// console.log(subsetSumBF([1, 4, 2, 5, 3, 1, 2, 8], 35));
+// console.log(subsetSumBF([1, 4, 6, 5], 11));
+// console.log(subsetSumBF([1, 4, 6, 5], 3));
+// console.log(subsetSumBF([1, 2, 3, 4], 8));
+// console.log(subsetSumBF([1, 2, 3, 4], 10));
+// console.log(subsetSumBF([1, 3, 4, 8], 6));
+// console.log(subsetSumBF([1, 2, 7, 1, 5], 10));
