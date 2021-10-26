a6c45e507c9db24b4f7b55c2fd859f025a14819a
pdl39 <dhlee9111@gmail.com>
2021-10-25 22:19:16 -0400

    Update - add memoization solution

--- src/15-dp/04-min-subset-sum-difference.js
+++ src/15-dp/04-min-subset-sum-difference.js
@@ -17,7 +17,7 @@ Explanation: We can partition the given set into two subsets where minimum absol
 between the sum of numbers is '92'. Here are the two subsets: {1, 3, 4} & {100}. */
 
 
-// #1: Brute Force (my initial solution without looking at the solution).
+// #1-0: Brute Force (my initial solution without looking at the solution).
 // T: O(2^n) --> all subset combinations.
 // S: O(n) --> for recursion stack
 // where n = length of the input array.
@@ -26,48 +26,128 @@ const minSubsetSumDiffBF0 = (arr) => {
   arr.forEach(num => sum += num);
   targetSum = Math.ceil(sum / 2);
 
-  const subsetSum1 = targetSum - findMinDiff0(arr, 0, 0, targetSum);
+  const subsetSum1 = targetSum - findMinDiffBF0(arr, 0, 0, targetSum);
   const subsetSum2 = sum - subsetSum1;
+  console.log({subsetSum1, subsetSum2})
   return subsetSum2 - subsetSum1;
 }
 
-const findMinDiff0 = (arr, currentIdx, subsetSum, targetSum) => {
+const findMinDiffBF0 = (arr, currentIdx, subsetSum, targetSum) => {
   if (currentIdx >= arr.length) return targetSum - subsetSum;
 
   let minDiff1 = targetSum - subsetSum;
   if (subsetSum + arr[currentIdx] <= targetSum) {
-    minDiff1 = findMinDiff0(arr, currentIdx + 1, subsetSum + arr[currentIdx], targetSum);
+    minDiff1 = findMinDiffBF0(arr, currentIdx + 1, subsetSum + arr[currentIdx], targetSum);
   }
-  const minDiff2 = findMinDiff0(arr, currentIdx + 1, subsetSum, targetSum);
+  const minDiff2 = findMinDiffBF0(arr, currentIdx + 1, subsetSum, targetSum);
 
   return Math.min(minDiff1, minDiff2);
 }
 
-// #2: Brute Force (the solution)
+// #1-1: Brute Force (the solution)
 // T: O(2^n) --> all subset combinations.
 // S: O(n) --> for recursion stack
 // where n = length of the input array.
 const minSubsetSumDiffBF = (arr) => {
-  return findMinDiff(arr, 0, 0, 0);
+  return findMinDiffBF(arr, 0, 0, 0);
 }
 
-const findMinDiff = (arr, currentIdx, subsetSum1, subsetSum2) => {
+const findMinDiffBF = (arr, currentIdx, subsetSum1, subsetSum2) => {
   if (currentIdx >= arr.length) return Math.abs(subsetSum1 - subsetSum2);
 
-  const minDiff1 = findMinDiff(arr, currentIdx + 1, subsetSum1 + arr[currentIdx], subsetSum2);
-  const minDiff2 = findMinDiff(arr, currentIdx + 1, subsetSum1, subsetSum2 + arr[currentIdx]);
+  const minDiff1 = findMinDiffBF(arr, currentIdx + 1, subsetSum1 + arr[currentIdx], subsetSum2); // min diff from subsets that include the current number.
+  const minDiff2 = findMinDiffBF(arr, currentIdx + 1, subsetSum1, subsetSum2 + arr[currentIdx]); // min diff from subsets that exclude the current number.
 
   return Math.min(minDiff1, minDiff2);
 }
+
+
+// #2-0: Memoization
+// T: O(n * s)
+// S: O(n * s)
+// where n = input array length.
+
+const minSubsetSumDiffMemo0 = (arr) => {
+  let sum = 0;
+  arr.forEach(num => sum += num);
+
+  const targetSum = sum / 2;
+
+  const dp = new Array(arr.length).fill(null)
+  .map(() => new Object());
+
+  const minDiff =  findMinDiffMemo0(arr, 0, 0, targetSum, dp);
+  const subsetSum1 = targetSum - minDiff;
+  const subsetSum2 = sum - subsetSum1;
+  console.log({subsetSum1, subsetSum2})
+  return subsetSum2 - subsetSum1;
+}
+
+const findMinDiffMemo0 = (arr, currentIdx, subsetSum, targetSum, dp) => {
+  if (currentIdx >= arr.length) return targetSum - subsetSum;
+
+  if (subsetSum in dp[currentIdx]) return dp[currentIdx][subsetSum];
+
+  let minDiff1 = targetSum - subsetSum;
+  if (subsetSum + arr[currentIdx] <= targetSum) {
+    minDiff1 = findMinDiffMemo0(arr, currentIdx + 1, subsetSum + arr[currentIdx], targetSum, dp);
+  }
+  const minDiff2 = findMinDiffMemo0(arr, currentIdx + 1, subsetSum, targetSum, dp);
+
+  dp[currentIdx][subsetSum] = Math.min(minDiff1, minDiff2);
+  return dp[currentIdx][subsetSum];
+}
+
+// #2-1: Memoization 2
+// T: O(n * s)
+// S: O(n * s)
+// where n = input array length.
+
+const minSubsetSumDiffMemo = (arr) => {
+  const dp = new Array(arr.length).fill(null)
+  .map(() => new Object());
+
+  return findMinDiffMemo(arr, 0, 0, 0, dp);
+}
+
+const findMinDiffMemo = (arr, currentIdx, subsetSum1, subsetSum2, dp) => {
+  if (currentIdx >= arr.length) return Math.abs(subsetSum1 - subsetSum2);
+
+  if (subsetSum1 in dp[currentIdx]) return dp[currentIdx][subsetSum1];
+
+  const minDiff1 = findMinDiffMemo(arr, currentIdx + 1, subsetSum1 + arr[currentIdx], subsetSum2, dp);
+  const minDiff2 = findMinDiffMemo(arr, currentIdx + 1, subsetSum1, subsetSum2 + arr[currentIdx], dp);
+
+  dp[currentIdx][subsetSum1] = Math.min(minDiff1, minDiff2);
+  return dp[currentIdx][subsetSum1];
+}
+
 
 
 // TEST
 console.log(minSubsetSumDiffBF0([1, 2, 3, 9]));
 console.log(minSubsetSumDiffBF0([1, 2, 7, 1, 5]));
 console.log(minSubsetSumDiffBF0([1, 3, 100, 4]));
+console.log(minSubsetSumDiffBF0([5, 7, 10, 1, 67, 82, 33]));
 
 console.log('----------------------');
 
 console.log(minSubsetSumDiffBF([1, 2, 3, 9]));
 console.log(minSubsetSumDiffBF([1, 2, 7, 1, 5]));
 console.log(minSubsetSumDiffBF([1, 3, 100, 4]));
+console.log(minSubsetSumDiffBF([5, 7, 10, 1, 67, 82, 33]));
+
+console.log('----------------------');
+
+console.log(minSubsetSumDiffMemo([1, 2, 3, 9]));
+console.log(minSubsetSumDiffMemo([1, 2, 7, 1, 5]));
+console.log(minSubsetSumDiffMemo([1, 3, 100, 4]));
+console.log(minSubsetSumDiffMemo([5, 7, 10, 1, 67, 82, 33]));
+
+console.log('----------------------');
+
+console.log(minSubsetSumDiffMemo0([1, 2, 3, 9]));
+console.log(minSubsetSumDiffMemo0([1, 2, 7, 1, 5]));
+console.log(minSubsetSumDiffMemo0([1, 3, 100, 4]));
+console.log(minSubsetSumDiffMemo0([5, 7, 10, 1, 67, 82, 33]));
+
